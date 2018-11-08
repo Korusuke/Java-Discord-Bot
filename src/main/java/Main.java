a771b3a410572f141907b5c6afa3f9f0efe34237
Kunal <kunal.sonawane@somaiya.edu>
2018-11-08 21:32:19 +0530

    News query added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import io.github.ccincharge.newsapi.NewsApi;
+import io.github.ccincharge.newsapi.datamodels.Article;
+import io.github.ccincharge.newsapi.requests.RequestBuilder;
+import io.github.ccincharge.newsapi.responses.ApiArticlesResponse;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDABuilder;
@@ -6,11 +10,17 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Main extends ListenerAdapter {
+
+    NewsApi newsApi;
+    ApiArticlesResponse apiArt;
+    ArrayList <Article> newsArticle;
+
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = ""; // Hide it later
+        String token = "NTA4MDAxMzM3MzcyNDQyNjQ0.Dr44-A.9qunUXKsAxEnnRLmT_V2TnqOGeA"; // Hide it later
         builder.setToken(token);
         builder.addEventListener(new Main());
         builder.buildAsync();
@@ -18,20 +28,41 @@ public class Main extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
+        newsApi = new NewsApi("12ecefd220214b38902f8a9d7dc0beff"); // Hide it later
 
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle("Reply", "https://www.google.com");
-        eb.setColor(Color.white);
-        eb.setDescription("Lorem Ipsum");
-        eb.addField("My Field", "Lorem Ipsum", false);
-        eb.addBlankField(false);
-        eb.setAuthor("John Doe", "https://www.github.com/", "http://chittagongit.com//images/icon-urls/icon-urls-8.jpg");
-        eb.setFooter("Hala Madrid!", null);
-        eb.setImage("http://chittagongit.com//images/icon-urls/icon-urls-8.jpg");
+        String param[] = event.getMessage().getContentRaw().split(" ", 2);
+        if(param[0].equals("!news")){
 
-        System.out.println("We received a message from " + event.getAuthor() .getName() + ": " + event.getMessage().getContentDisplay());
-        if(event.getMessage().getContentRaw().equals("!ping")){
-            event.getChannel().sendMessage(eb.build()).queue();
+            System.out.println("We received a message from " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
+            System.out.println("Parameter passed: " + param[1]);
+
+            RequestBuilder query_request = new RequestBuilder().setQ(param[1]).setLanguage("en");
+            try{
+                apiArt = newsApi.sendTopRequest(query_request);
+            }
+            catch (Exception e){
+                System.out.println("Error :" + e.getMessage());
+            }
+            System.out.println("API Response: " +  apiArt.articles());
+
+            newsArticle = apiArt.articles();
+            for(Article x: newsArticle) {
+                EmbedBuilder eb = new EmbedBuilder();
+
+                try{
+                    eb.setTitle(x.title(), x.url());
+                    eb.setColor(Color.white);
+                    eb.setDescription(x.description());
+                    eb.setAuthor(x.author(), null, x.urlToImage());
+                    eb.setFooter("Published at " + x.publishedAt(), null);
+                    eb.setThumbnail(x.urlToImage());
+                    event.getChannel().sendMessage(eb.build()).queue();
+                }
+                catch(Exception e){
+                    System.out.println("Error :" + e.getMessage());
+                }
+            }
+            System.out.println("Done with given query.");
         }
     }
 }
