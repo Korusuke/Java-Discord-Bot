76d4ff5fd6cff4ab62e2643a996f698feabb7391
Karan Sheth <karan.sheth@somaiya.edu>
2018-11-10 20:50:03 +0530

    Hopefully Final try

--- run_heroku.sh
+++ run_heroku.sh
@@ -6,6 +6,7 @@ export ENABLE_WEB=false
 LATEST_BUILD="$(find ./build/libs | sort --version-sort --field-separator=- --key=2,2 | tail -n 1)"
 
 until java -jar "${LATEST_BUILD}"; do
+    echo "${LATEST_BUILD}"
     echo "BanUtil crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
